b140431f4d9578d0099c03fddda38a6c1f7b8dff
Carlos <contact@carloscolin.me>
2021-11-11 22:43:53 -0600

    Category view initial commit

---
+++ src/views/Category.vue
@@ -0,0 +1,37 @@
+<template>
+  <BasicLayout>
+    <h1>Category</h1>
+  </BasicLayout>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import BasicLayout from '@/layouts/BasicLayout.vue';
+
+export default {
+  name: 'Category',
+  components: {
+    BasicLayout,
+  },
+  watch: {
+    $route(to) {
+      console.log('to', to.params.category_slug);
+    },
+  },
+  setup() {
+    const category = ref('');
+    const route = useRoute();
+
+    onMounted(() => {
+      category.value = route.params.category_slug;
+    });
+
+    return {};
+  },
+};
+</script>
+
+<style>
+
+</style>
