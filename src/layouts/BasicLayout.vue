d696b4b69ee4fcbec25db20f07517c37d3e5a1a3
Carlos <contact@carloscolin.me>
2021-11-13 23:27:19 -0600

    added cart to basic layout

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -6,15 +6,19 @@
         <slot />
     </div>
   </div>
+
+  <Cart />
 </template>
 
 <script>
 import Menu from '@/components/Menu.vue';
+import Cart from '@/components/Cart/Cart.vue';
 
 export default {
   name: 'BasicLayout',
   components: {
     Menu,
+    Cart,
   },
 };
 </script>
