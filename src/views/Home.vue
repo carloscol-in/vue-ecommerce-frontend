68ea5f50d73265d0124ea0c926aedac13388c648
Carlos <contact@carloscolin.me>
2021-11-13 23:33:12 -0600

    added products

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,35 @@
 <template>
   <BasicLayout>
-    <h2>We're at home</h2>
+    <h1>Latest products</h1>
+    <div class="ui grid">
+      <article
+        class="sixten wide mobile eight wide tablet four wide computer column"
+        v-for="product in products"
+        :key="product.id"
+      >
+        <Product :product="product" />
+      </article>
+    </div>
   </BasicLayout>
 </template>
 
 <script>
 import { ref, onMounted } from 'vue';
 import { getProducts } from '@/api/product';
+import Product from '@/components/Product.vue';
 import BasicLayout from '@/layouts/BasicLayout.vue';
 
 export default {
   name: 'Home',
   components: {
     BasicLayout,
+    Product,
   },
   setup() {
-    let products = ref(null);
-    let next = ref(0);
+    const products = ref(null);
 
     onMounted(async () => {
       const response = await getProducts(5, 0);
-      next.value = response.next;
       products.value = response;
     });
 
