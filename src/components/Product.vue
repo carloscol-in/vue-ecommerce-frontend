1679113b0293007823e9cd6dbdda1ccec5b19742
Carlos <contact@carloscolin.me>
2021-11-13 23:34:10 -0600

    product component

---
+++ src/components/Product.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="ui card product">
+    <div class="image">
+      <img :src="`${API_URL}${product.image[0].url}`" :alt="product.title">
+    </div>
+    <div class="content">
+      <div class="header">{{ product.title }}</div>
+      <div class="price">{{ product.price }}</div>
+    </div>
+    <div class="ui button primary" @click="addProductCart(product.slug)">
+      Comprar
+    </div>
+  </div>
+</template>
+
+<script>
+// import { ref } from 'vue';
+import { API_URL } from '@/utils/constants';
+import { addProductCartApi } from '@/api/cart';
+
+export default {
+  name: 'Product',
+  props: {
+    product: Object,
+  },
+  setup() {
+    const addProductCart = (slug) => {
+      addProductCartApi(slug);
+    };
+    return {
+      API_URL,
+      addProductCart,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.product {
+  &:hover {
+    .ui.button {
+      min-height: 36px;
+    }
+  }
+
+  .ui.button {
+    max-height: 0;
+    min-height: 0;
+    overflow: hidden;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: min-height 0.6s ease;
+  }
+}
+</style>
