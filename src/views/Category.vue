3250a3f04c9245d00461eb3a6ba8b6b62068e23b
Carlos <contact@carloscolin.me>
2021-11-13 23:32:53 -0600

    added category view

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,33 +1,67 @@
 <template>
   <BasicLayout>
-    <h1>Category</h1>
+    <h1>{{ categoryCap }}</h1>
+    <div class="ui grid">
+      <article
+        class="sixten wide mobile eight wide tablet four wide computer column"
+        v-for="product in products"
+        :key="product.id"
+      >
+        <Product :product="product" />
+      </article>
+    </div>
   </BasicLayout>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { useRoute } from 'vue-router';
+
+import { getProductCategory } from '@/api/product';
+
 import BasicLayout from '@/layouts/BasicLayout.vue';
+import Product from '@/components/Product.vue';
 
 export default {
   name: 'Category',
   components: {
     BasicLayout,
+    Product,
   },
   watch: {
     $route(to) {
-      console.log('to', to.params.category_slug);
+      this.populateProducts(to.params.category_slug);
     },
   },
   setup() {
+    const products = ref(null);
     const category = ref('');
-    const route = useRoute();
+    const { params } = useRoute();
 
-    onMounted(() => {
-      category.value = route.params.category_slug;
+    const populateProducts = async (categorySlug) => {
+      category.value = categorySlug;
+      const response = await getProductCategory(categorySlug, 5, 0);
+      products.value = response;
+    };
+
+    onMounted(async () => {
+      category.value = params.category_slug;
+      populateProducts(category.value);
     });
 
-    return {};
+    const capitalize = (str, lower = false) => (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) => match.toUpperCase());
+
+    let categoryCap = computed(() => {
+      let result = capitalize(category.value);
+      return result;
+    });
+
+    return {
+      category,
+      products,
+      categoryCap,
+      populateProducts,
+    };
   },
 };
 </script>
