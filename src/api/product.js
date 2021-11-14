0e38df2fcae16e6d87972687113b17903a8e1cdc
Carlos <contact@carloscolin.me>
2021-11-13 23:22:01 -0600

    added product functions

--- src/api/product.js
+++ src/api/product.js
@@ -1,16 +1,35 @@
 import { API_URL } from '@/utils/constants';
 
 export async function getProducts(limit = 5, start = 0) {
-  const next = limit + start;
   try {
     const response = await fetch(
       `${API_URL}/products?_sort=created_at:desc&_limit=${limit}&_start=${start}`,
     );
-    let result = await response.json();
-    result = {
-      ...result,
-      next,
-    };
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export async function getProductCategory(category, limit = 5, start = 0) {
+  try {
+    const response = await fetch(
+      `${API_URL}/products?_where[category.slug]=${category}&_sort=created_at:desc&_limit=${limit}&_start=${start}`,
+    );
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export async function getProductApi(slug) {
+  try {
+    const response = await fetch(`${API_URL}/products/${slug}`);
+    const result = await response.json();
     return result;
   } catch (error) {
     console.log(error);
